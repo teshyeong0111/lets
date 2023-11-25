3bf495cada46f0f54e50568b0ba9544ac3480383
teshyeong0111 <siwoo@letscoding.kr>
2023-11-25 11:51:03 +0900

    정보전다

--- edu/views.py
+++ edu/views.py
@@ -1,5 +1,5 @@
 from django.views.generic import View
-from django.shortcuts import get_object_or_404, render
+from django.shortcuts import get_object_or_404, render, redirect
 
 class Index(View):
     template_name = 'index.html'
@@ -12,3 +12,14 @@ class TagStudy(View):
 
     def get(self, request):
         return render(request, self.template_name)
+    
+class NewContent(View):
+    template_name = 'upload_form.html'
+
+    def get(self, request):
+        return render(request, self.template_name)
+    
+    def post(self, reguest):
+        param = reguest.POST.get('content','')
+        print(f"param:{param}")
+        return redirect('edu:tag_study')
