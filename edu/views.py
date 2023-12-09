f516449fb7cf2a3d150f0047760a2faeb0e9bb47
teshyeong0111 <siwoo@letscoding.kr>
2023-12-09 11:48:54 +0900

    이미지 업로드

--- edu/views.py
+++ edu/views.py
@@ -1,6 +1,6 @@
 from django.views.generic import View
 from django.shortcuts import get_object_or_404, render, redirect
-
+from .models import Feed
 class Index(View):
     template_name = 'index.html'
 
@@ -21,5 +21,8 @@ class NewContent(View):
     
     def post(self, reguest):
         param = reguest.POST.get('content','')
+        param2 = reguest.FILES.get('up_photo', False)
         print(f"param:{param}")
+        feed = Feed(content = param, photo=param2)
+        feed.save()
         return redirect('edu:tag_study')
